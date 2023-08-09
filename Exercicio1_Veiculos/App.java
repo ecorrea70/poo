93aca1033854ace29e6db2d41e42b07bbc50b989
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-08-09 16:52:42 -0300

    exercicio1

---
+++ Exercicio1_Veiculos/App.java
@@ -0,0 +1,36 @@
+public class App{
+    public static void main(String args[]){
+        // Placa p1 = new Placa("Brasil","ABC1R23");
+        
+        // Veiculo v = new Veiculo(p1);
+        // System.out.println("Dados do veiculo antes de abastecer:");
+        // System.out.println(v);
+        // System.out.println("----------------");
+
+        // v.abastece(65);
+        // System.out.println("Dados do veiculo depois de abastecer:");
+        // System.out.println(v);
+        // System.out.println("----------------");
+
+        // v.dirige(250);
+        // System.out.println("Dados do veiculo depois de percorrer 250Km:");
+        // System.out.println(v);
+        // System.out.println("----------------");
+        // }
+
+//         Reescreva o método "main" da seguinte forma: crie um arranjo e
+//          armazene no mesmo 5 veículos diferentes. Apresente para o
+// usuário um menu com três opções: "1) Abastecer", "2) dirigir" e "3)
+// Fim". A opção 1 solicita a placa do veículo a ser abastecido e a
+// quantidade de combustível e, ao final da operação, exibe na tela a
+// quantidade total de combustível no veículo após o abastecimento.
+// A opção 2 solicita a placa do veículo que o usuário pretende dirigir
+// e a distância a ser percorrida e, ao final da operação, imprime na
+// tela a distância efetivamente percorrida e a quantidade de
+// combustível remanescente no tanque. Tanto a opção 1 como a
+// opção 2 devem exibir a mensagem "Veiculo não encontrado" caso
+// a placa informada não conste do cadastro (vetor). A opção 3 deve
+// imprimir os dados de todos os veículos cadastrados e encerrar o
+// programa.
+
+    }}
